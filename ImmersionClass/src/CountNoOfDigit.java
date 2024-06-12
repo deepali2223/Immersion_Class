3f5fa15eae321a07d387298ed8a3d930c1bdbc6b
deepali2223 <deepalibisht3244@gmail.com>
2024-06-12 19:22:45 +0530

    new changes

---
+++ ImmersionClass/src/CountNoOfDigit.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class CountNoOfDigit {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int noOfDigit = (int)(Math.log10(num)+1);
+        System.out.println("No of digit ="+ noOfDigit);
+        //brute force
+        int ans = findLen(num);
+        System.out.println(ans);
+    }
+
+    private static int findLen(int num) {
+        int c = 0;
+        while(num >0){
+            int rem = num%10;
+            c++;
+            num /=10;
+        }
+        return c;
+    }
+}
